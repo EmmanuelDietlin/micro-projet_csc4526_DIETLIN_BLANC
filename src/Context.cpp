634470f6fc6690a95502e5bf143d67f3d076fb39
Emmanuel Dietlin <emmanuel.dietlin@telecom-sudparis.eu>
2022-05-26 11:18:07 +0200

    début de l'ajout de méthodes pour la classe contexte, qui contient ainsi les stratégies pour les actions & les évents

--- src/Context.cpp
+++ src/Context.cpp
@@ -1,1 +1,14 @@
 #include "Context.h"
+
+Context::Context() {
+	action = nullptr;
+	evnt = nullptr;
+}
+
+void Context::setAction(std::unique_ptr<Action> && action) {
+	action = std::move(action);
+}
+
+void Context::setEvent(std::unique_ptr<Event>&& evnt) {
+	evnt = std::move(evnt);
+}
--- src/Context.h
+++ src/Context.h
@@ -1,7 +1,14 @@
-
+#include "Action.h"
+#include "Event.h"
+#include <memory>
 
 class Context {
 private:
-
+	std::unique_ptr<Action> action;
+	std::unique_ptr<Event> evnt;
 public:
+	Context();
+	void setAction(std::unique_ptr<Action> &&action);
+	void setEvent(std::unique_ptr<Event> && evnt);
+
 };

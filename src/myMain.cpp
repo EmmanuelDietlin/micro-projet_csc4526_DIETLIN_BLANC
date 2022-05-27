cdc909a9c3d80ec157ca53b09b5f57cb7449289b
Emmanuel Dietlin <emmanuel.dietlin@telecom-sudparis.eu>
2022-05-27 17:02:06 +0200

    ajout du code pour tester l'affichage avec tmxlite

--- src/myMain.cpp
+++ src/myMain.cpp
@@ -18,9 +18,16 @@ const int w_width = 1000;
 int myMain()
 {
     sf::RenderWindow window(sf::VideoMode(w_width, w_height), "SFML ");
+    ImGui::SFML::Init(window);
+    sf::Clock globalClock;
     sf::Clock deltaClock;
 
     tmx::Map map;
+    map.load("resources/game_background.tmx");
+
+    MapLayer layerZero(map, 0);
+    MapLayer layerOne(map, 1);
+    MapLayer layerTwo(map, 2);
 
     while (window.isOpen()) {
         sf::Event event;
@@ -30,7 +37,18 @@ int myMain()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+        sf::Time duration = globalClock.getElapsedTime();
+        layerZero.update(duration);
         ImGui::SFML::Update(window, deltaClock.restart());
+
+
+        window.clear(sf::Color::Black);
+        window.draw(layerZero);
+        window.draw(layerOne);
+        window.draw(layerTwo);
+        ImGui::SFML::Render(window);
+        window.display();
+
     }
     ImGui::SFML::Shutdown();
     return 0;

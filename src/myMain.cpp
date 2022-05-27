41c5d9572464eea97747baf05ae9d4479d1eaedf
Emmanuel Dietlin <emmanuel.dietlin@telecom-sudparis.eu>
2022-05-27 17:18:29 +0200

    test pour créer un bouton via sfml

--- src/myMain.cpp
+++ src/myMain.cpp
@@ -8,7 +8,9 @@
 #include <thread>
 #include <imgui.h>
 #include <imgui-SFML.h>
+#include <iostream>
 #include "SFMLOrthogonalLayer.h"
+
 
 const int w_height = 800;
 const int w_width = 1000;
@@ -29,6 +31,9 @@ int myMain()
     MapLayer layerOne(map, 1);
     MapLayer layerTwo(map, 2);
 
+    sf::RectangleShape button1(sf::Vector2f(50, 20));
+    button1.setPosition(100, 100);
+
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event))
@@ -36,6 +41,11 @@ int myMain()
             ImGui::SFML::ProcessEvent(window, event);
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (event.type == sf::Event::MouseButtonReleased) {
+                if (button1.getGlobalBounds().contains(window.mapPixelToCoords(sf::Mouse::getPosition()))) {
+                    std::cout << "Button clicked" << std::endl;
+                }
+            }
         }
         sf::Time duration = globalClock.getElapsedTime();
         layerZero.update(duration);
@@ -46,6 +56,7 @@ int myMain()
         window.draw(layerZero);
         window.draw(layerOne);
         window.draw(layerTwo);
+        window.draw(button1);
         ImGui::SFML::Render(window);
         window.display();
 
